5e442aba9a07c022b69fd1c2e249d64e32ab26d9
Alexis Soltysiak <138500067+alexis-soltysiak@users.noreply.github.com>
2024-01-02 15:48:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from matplotlib.animation import FuncAnimation
 ########################################################
 
 def complex_function(x):
-    return 3 * x * x + 2 * x + 6
+    return 3 * x * x + 2 * x + 4
 
 
 ########################################################
